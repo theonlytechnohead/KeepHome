73905bf71f7bd72db5f8bebc825990cc9eac7625
Craig Anderson <cranderson0110@gmail.com>
2021-11-26 17:38:55 +1300

    Refactored
    And a button bar something

--- app/src/main/java/net/ddns/anderserver/keephome/MainActivity.kt
+++ app/src/main/java/net/ddns/anderserver/keephome/MainActivity.kt
@@ -3,9 +3,9 @@ package net.ddns.anderserver.keephome
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,25 +29,55 @@ class MainActivity : ComponentActivity() {
 fun Layout() {
     KeephomeTheme {
         Scaffold(
-            topBar = { CenterAlignedTopAppBar(
-                title = { Text(text = "KeepHome")}
-            ) }
+            topBar = { ActionBar() }
         ) {
-            Column {
-
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    Column {
-                        Row(Modifier.padding(10.dp)) {
-                            Greeting(name = "Jean")
-                        }
-                        Row(Modifier.padding(10.dp)) {
-                            Button(onClick = { /*TODO*/ }) {
-                                Text(text = "Click me!")
-                            }
-                        }
-                    }
+            Surface(color = MaterialTheme.colorScheme.background) {
+                Column {
+                    ButtonBar()
+                    Content()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun ActionBar() {
+    SmallTopAppBar(
+        title = { Text(text = "KeepHome") },
+        actions = { AppBarActions() }
+    )
+}
+
+@Composable
+fun AppBarActions() {
+    IconButton(onClick = { /*TODO*/ }) {
+        Icon(Icons.Filled.Settings, contentDescription = "")
+    }
+}
+
+@Composable
+fun ButtonBar() {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(10.dp, 0.dp)
+            .fillMaxWidth()
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Refresh")
+        }
+    }
+}
+
+@Composable
+fun Content() {
+    Row(Modifier.padding(10.dp)) {
+        Greeting(name = "Jean")
+    }
+    Row(Modifier.padding(10.dp)) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Click me!")
         }
     }
 }
