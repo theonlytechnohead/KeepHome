2eb749ffd30ddba8a60aa0c690c125acdb0ba000
Craig Anderson <cranderson0110@gmail.com>
2021-11-30 20:57:11 +1300

    Toggle switches!
    And layout tweaks to look better

--- app/src/main/java/net/ddns/anderserver/keephome/SettingsActivity.kt
+++ app/src/main/java/net/ddns/anderserver/keephome/SettingsActivity.kt
@@ -1,21 +1,22 @@
 package net.ddns.anderserver.keephome
 
 import android.os.Bundle
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Divider
+import androidx.compose.material.Switch
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -64,6 +65,8 @@ class SettingsActivity : ComponentActivity() {
             WiFiSettings()
             HorizontalLine()
             AdvancedSettings()
+            HorizontalLine()
+            SupportSettings()
         }
     }
 
@@ -71,7 +74,7 @@ class SettingsActivity : ComponentActivity() {
     fun SectionTitle(title: String) {
         Row(
             Modifier
-                .padding(60.dp, 10.dp)
+                .padding(60.dp, 15.dp, 0.dp, 10.dp)
                 .fillMaxWidth()
         ) {
             Text(
@@ -89,7 +92,12 @@ class SettingsActivity : ComponentActivity() {
 //    }
 
     @Composable
-    fun Setting(title: String, description: String, onClick: (() -> Unit)? = null) {
+    fun Setting(
+        title: String,
+        description: String,
+        onClick: (() -> Unit)? = null,
+        toggle: (@Composable() () -> Unit)? = null
+    ) {
         Row(
             Modifier
                 .fillMaxWidth()
@@ -97,24 +105,46 @@ class SettingsActivity : ComponentActivity() {
                     interactionSource = remember { MutableInteractionSource() },
                     indication = rememberRipple(bounded = true),
                     onClick = { if (onClick != null) onClick() }
-                )
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Column(Modifier.padding(60.dp, 10.dp, 0.dp, 10.dp)) {
-                Row {
-                    Text(
-                        text = title,
-                        style = MaterialTheme.typography.titleMedium,
-                        color = MaterialTheme.colorScheme.onBackground,
-                    )
-                }
-                Row {
-                    Text(
-                        text = description,
-                        style = MaterialTheme.typography.titleSmall,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                    )
+            Column(
+                Modifier
+                    .padding(60.dp, 18.dp, 0.dp, 18.dp)
+                    .weight(1f, fill = false)) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Text(
+                    text = description,
+                    style = MaterialTheme.typography.titleSmall,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+            if (toggle != null) {
+                Column(
+                    Modifier.requiredWidth(80.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    toggle()
                 }
             }
+        }
+    }
+
+    @Composable
+    fun ToggleSetting(title: String, description: String) {
+        val toggled = remember { mutableStateOf(false) }
+        Setting(
+            title = title,
+            description = description,
+            onClick = {toggled.value = !toggled.value}
+        ) {
+            Switch(checked = toggled.value, onCheckedChange = { toggled.value = it })
         }
     }
 
@@ -122,21 +152,24 @@ class SettingsActivity : ComponentActivity() {
     fun HorizontalLine() {
         Row(Modifier.padding(0.dp, 5.dp)) {
             Divider(
-                thickness = 1.dp,
+                thickness = 1.dp
             )
         }
     }
 
-//    @Preview
-//    @Composable
-//    fun SettingPreview() {
-//        Setting(title = "Demo setting", description = "Description goes here")
-//    }
+    @Preview
+    @Composable
+    fun ToggleSettingPreview() {
+        ToggleSetting(
+            title = "Demo setting",
+            description = "Description goes here, and this is a really long one too!"
+        )
+    }
 
     @Composable
     fun SyncSettings() {
         SectionTitle(title = "Sync / Notifications")
-        Setting(
+        ToggleSetting(
             title = "Enable sync notifications",
             description = "You won't be disturbed by notifications"
         )
@@ -152,7 +185,7 @@ class SettingsActivity : ComponentActivity() {
     @Composable
     fun WiFiSettings() {
         SectionTitle(title = "KeepHome WiFi")
-        Setting(
+        ToggleSetting(
             title = "Access point mode",
             description = "KeepHome will connect to your own network"
         )
@@ -164,6 +197,12 @@ class SettingsActivity : ComponentActivity() {
     fun AdvancedSettings() {
         SectionTitle(title = "Advanced")
         Setting(title = "KeepHome IP", description = "192.168.4.1")
+    }
+
+    @Composable
+    fun SupportSettings() {
+        SectionTitle(title = "Support")
+        Setting(title = "Website", description = "Submit issues and requests")
     }
 
 }
