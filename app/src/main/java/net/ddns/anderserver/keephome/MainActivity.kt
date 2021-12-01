94f05a0a6678e84fb1846672b17a3deba9cda545
Craig Anderson <cranderson0110@gmail.com>
2021-12-02 12:26:59 +1300

    Oops, the `MainActivity` @Composable's were outside the class!
    And now it's time for a proper main UI, now that the settings are all implemented and changeable

--- app/src/main/java/net/ddns/anderserver/keephome/MainActivity.kt
+++ app/src/main/java/net/ddns/anderserver/keephome/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,6 +18,9 @@ import androidx.compose.ui.unit.dp
 import net.ddns.anderserver.keephome.ui.theme.KeephomeTheme
 
 class MainActivity : ComponentActivity() {
+
+    val result = mutableStateOf("")
+
     @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,69 +28,75 @@ class MainActivity : ComponentActivity() {
             Layout()
         }
     }
-}
 
-@ExperimentalMaterial3Api
-@Preview(showBackground = true)
-@Composable
-fun Layout() {
-    KeephomeTheme() {
-        Scaffold(
-            topBar = { ActionBar() }
-        ) {
-            Surface(color = MaterialTheme.colorScheme.background) {
-                Column {
-                    ButtonBar()
-                    Content()
+
+    @ExperimentalMaterial3Api
+    @Preview(showBackground = true)
+    @Composable
+    fun Layout() {
+        KeephomeTheme() {
+            Scaffold(
+                topBar = { ActionBar() }
+            ) {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Column {
+                        ButtonBar()
+                        Content()
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun ActionBar() {
-    SmallTopAppBar(
-        title = { Text(text = "KeepHome") },
-        actions = { AppBarActions() }
-    )
-}
-
-@Composable
-fun AppBarActions() {
-    val context = LocalContext.current
-    IconButton(onClick = { context.startActivity(Intent(context, SettingsActivity::class.java)) }) {
-        Icon(Icons.Filled.Settings, contentDescription = "Settings")
+    @Composable
+    fun ActionBar() {
+        SmallTopAppBar(
+            title = { Text(text = "KeepHome") },
+            actions = { AppBarActions() }
+        )
     }
-}
 
-@Composable
-fun ButtonBar() {
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .padding(10.dp, 0.dp)
-            .fillMaxWidth()
-    ) {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Refresh")
+    @Composable
+    fun AppBarActions() {
+        val context = LocalContext.current
+        IconButton(onClick = {
+            context.startActivity(
+                Intent(
+                    context,
+                    SettingsActivity::class.java
+                )
+            )
+        }) {
+            Icon(Icons.Filled.Settings, contentDescription = "Settings")
         }
     }
-}
 
-@Composable
-fun Content() {
-    Row(Modifier.padding(10.dp)) {
-        Greeting(name = "Jean")
-    }
-    Row(Modifier.padding(10.dp)) {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "Click me!")
+    @Composable
+    fun ButtonBar() {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(10.dp, 0.dp)
+                .fillMaxWidth()
+        ) {
+            Button(onClick = {
+                refreshContent()
+            }) {
+                Text(text = "Refresh")
+            }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    fun refreshContent() {
+        result.value = "Result!"
+    }
+
+    @Composable
+    fun Content() {
+        val text by result
+        Row(Modifier.padding(10.dp)) {
+            Text(text = text)
+        }
+    }
+
 }
