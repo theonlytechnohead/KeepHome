83640100af13cd10d15b56211f83306c0bdf8cf5
Craig Anderson <cranderson0110@gmail.com>
2021-12-02 19:33:23 +1300

    mDNS / DNS-SD / Bonjour added!
    Feature parity with the old app is almost there. Just gotta propagate the settings through to KeepHome, and do the background sync thing.

--- app/src/main/java/net/ddns/anderserver/keephome/MainActivity.kt
+++ app/src/main/java/net/ddns/anderserver/keephome/MainActivity.kt
@@ -11,20 +11,41 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.github.druk.rx2dnssd.BonjourService
+import com.github.druk.rx2dnssd.Rx2DnssdBindable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 import net.ddns.anderserver.keephome.ui.theme.KeephomeTheme
 import net.ddns.anderserver.keephome.ui.theme.SettingsStore
 import org.json.JSONObject
 
+
 class MainActivity : ComponentActivity() {
 
-    private var result = mutableStateOf("{}")
+    enum class ContentState {
+        BLANK, ERROR, INFO
+    }
+
+    private val state = mutableStateOf(ContentState.BLANK)
+    private val online = mutableStateOf(false)
+    private var result = "{}"
+
+    private lateinit var browseDisposable: Disposable
+    private lateinit var rx2dnssd: Rx2DnssdBindable
+
 
     @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +53,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             Layout()
         }
+        rx2dnssd = Rx2DnssdBindable(applicationContext)
+        val settings = SettingsStore(applicationContext)
+        browseDisposable = rx2dnssd.browse(
+            "_http._tcp",
+            "local."
+        )
+            .compose(rx2dnssd.resolve())
+            .compose(rx2dnssd.queryIPV4Records())
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { service: BonjourService ->
+                if (service.serviceName.contains("KeepHome")) {
+                    if (!service.isLost) {
+                        val ip = service.inet4Address!!.hostAddress ?: "192.168.4.1"
+                        lifecycleScope.launch {
+                            settings.setIP(ip)
+                        }
+                        online.value = true
+                    } else {
+                        online.value = false
+                    }
+                }
+            }
     }
 
 
@@ -39,7 +83,7 @@ class MainActivity : ComponentActivity() {
     @Preview(showBackground = true)
     @Composable
     fun Layout() {
-        KeephomeTheme() {
+        KeephomeTheme {
             Scaffold(
                 topBar = { ActionBar() }
             ) {
@@ -64,6 +108,10 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun AppBarActions() {
         val context = LocalContext.current
+        if (online.value)
+            Text(text = "Online", color = MaterialTheme.colorScheme.secondary)
+        else
+            Text(text = "Offline", color = MaterialTheme.colorScheme.outline)
         IconButton(onClick = {
             context.startActivity(
                 Intent(
@@ -101,10 +149,12 @@ class MainActivity : ComponentActivity() {
                 Request.Method.POST,
                 "http://$address/post",
                 {
-                    result.value = it
+                    state.value = ContentState.INFO
+                    result = it
                 },
                 {
-                    result.value = "Error: $it"
+                    state.value = ContentState.ERROR
+                    result = it.toString()
                 }
             )
         )
@@ -112,16 +162,44 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun Content() {
-        val json = JSONObject(result.value)
-        var time = ""
-        if (json.has("time")) time = json["time"].toString()
-        var additional = ""
-        if (json.has("additional")) additional = json["additional"].toString()
-        Row(Modifier.padding(10.dp)) {
-            Text(text = "Uptime: ${time}s")
-        }
-        Row(Modifier.padding(10.dp)) {
-            Text(text = "Additional info: $additional")
+        when (state.value) {
+            ContentState.BLANK -> {
+                Column(
+                    Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Use the 'Refresh' button to get info",
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+            }
+            ContentState.ERROR -> {
+                Column(
+                    Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "Error!", fontWeight = FontWeight.Bold)
+                    Text(text = result, fontStyle = FontStyle.Italic)
+                }
+            }
+            ContentState.INFO -> {
+                val json = JSONObject(result)
+                var time = ""
+                if (json.has("time")) time = json["time"].toString()
+                var additional = ""
+                if (json.has("additional")) additional = json["additional"].toString()
+                Column(
+                    Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(text = "Uptime: ${time}s")
+                    Text(text = "Additional info: $additional")
+                }
+            }
         }
     }
 
